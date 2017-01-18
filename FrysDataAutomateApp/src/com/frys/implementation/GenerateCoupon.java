cf2c574078163b87f7f4107de215a6e00b4b3963
GagandeepSinghSaini <g.saini2692@gmail.com>
2017-01-17 17:31:17 -0800

    FrysDataAutomation App  first commit local

---
+++ FrysDataAutomateApp/src/com/frys/implementation/GenerateCoupon.java
@@ -0,0 +1,20 @@
+package com.frys.implementation;
+
+import org.apache.log4j.Logger;
+
+import com.frys.interfaces.DBHelper;
+import com.frys.interfaces.FrysCouponProcess;
+import com.frys.interfaces.FrysCouponType;
+import com.frys.interfaces.FrysProcess;
+
+public class GenerateCoupon implements FrysCouponProcess{
+
+	private Logger log = Logger.getLogger(GenerateCoupon.class);
+
+	@Override
+	public boolean service(FrysCouponType couponType) {
+		return couponType.generateCouponService();
+	}
+	
+	
+}
